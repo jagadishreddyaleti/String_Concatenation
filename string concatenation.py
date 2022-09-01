2d18944e3bc9d8aa995743542d6b34fe20867254
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-09-01 11:38:15 +0530

    Add files via upload

---
+++ string concatenation.py
@@ -0,0 +1,42 @@
+"   string concatenation  "
+
+# a=3 ;b=5
+# print(a+b)
+# a=4;b=6.7
+# print(a+b)
+
+# x='hi';y=5;z=6.7
+# # print(x+y)
+# # print(x+z)
+
+# a='he';b='y'
+# print(a+b)
+# print(a+a)
+# print(b+a)
+
+# a='python';b=3;c='3.10.6'
+# print(a+str(b))
+# print(a+c)
+
+# print(''+'hi')
+# print(' '+'hi')
+ 
+# print('hello'+''+'python')
+# print('hello'+' '+'python')
+# print('hello'+'  '+'python')
+# print('hello'+'    '+'python')
+
+# # print('py','3')
+# print('hello'+'we are using python version'+'3.10.6')
+# print('hello','we are using python version','3.10.6')
+# print('hello'+' '+'we are using python version'+' '+'3.10.6')
+
+# print('hi'+' '+str(5))
+
+print(1,2,3,4,5)
+print('1'+' '+'2'+' '+'3'+' '+'4'+' '+'5')
+print(1,2,3,4,5,sep='')
+print(1,2,3,4,5,sep='  ')
+print(1,2,3,4,5,sep=' - ')
+print(1,2,3,4,5,sep=' -> ')
+ 
